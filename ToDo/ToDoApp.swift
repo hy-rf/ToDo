f6709cd812b4ff825599bce34b190e5c1cfbd6dc
hy-rf <heyu880202@gmail.com>
2024-07-23 16:16:55 +0800

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by hyrf on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
